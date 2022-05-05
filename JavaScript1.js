250d4dfa19187930a7fe7b4a922c838e8c3ed5dc
Laj111 <104667470+Laj111@users.noreply.github.com>
2022-05-05 22:42:07 +0800

    Update JavaScript1.js

--- JavaScript1.js
+++ JavaScript1.js
@@ -8,6 +8,16 @@
 		window.document.f.action = "/关雎.html";  //此处设置登录后跳转页面
 		window.document.f.submit();
 	}
+	else if (name == "gt" && pass == "941009"){
+		alert("登录成功！");
+		window.document.f.action = "/gt.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
+	else if (name == "whl" && pass == "xxxxxxxx"){
+		alert("登录成功！");
+		window.document.f.action = "/whl.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
 
 	else {
 		alert("用户名或密码错误！");
