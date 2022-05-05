b64ef72d013348d00b76f074c2e79a35ce3a89b4
Laj111 <104667470+Laj111@users.noreply.github.com>
2022-05-05 20:59:04 +0800

    Update JavaScript1.js

--- JavaScript1.js
+++ JavaScript1.js
@@ -13,6 +13,11 @@
 		window.document.f.action = "/gt.html";  //此处设置登录后跳转页面
 		window.document.f.submit();
 	}
+	 else if(name == "whl" && pass == "2002"){
+		alert("登录成功！");
+		window.document.f.action = "/whl.html";  //此处设置登录后跳转页面
+		window.document.f.submit();
+	}
 	 
 	else {
 		alert("用户名或密码错误！");
